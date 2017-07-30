e816339b8fc0bbc960ee09b7db6d97bc944ad2a2
webprogra <1552917447@qq.com>
2017-07-30 22:12:37 +0800

    Create main.rb

---
+++ B1/main.rb
@@ -0,0 +1,44 @@
+require './message.rb'
+require 'sinatra'
+get '/' do
+  message=Message.new
+  @messages=message.getMessage()
+  erb:show
+end
+post '/add' do
+  if params[:author]==nil||params[:message].length<10
+    redirect to('add_error')
+  end
+  message=Message.new
+  @messages=message.addMessage(params[:author],params[:message])
+  erb:show
+end
+post '/delete' do
+  message=Message.new
+  @messages=message.deleteMessage(params[:id])
+  erb:show
+end
+get '/check_id' do
+  message=Message.new
+  @message=message.checkMessageId(params[:id])
+  if @message==nil||@message.length==0
+    redirect to('/check_error')
+  else
+    erb:check
+  end
+end
+get '/check_author' do
+  message=Message.new
+  @message=message.checkMessageAuthor(params[:author])
+  if @message==nil||@message.length==0
+    redirect to('/check_error')
+  else
+    erb:check
+  end
+end
+get '/add_error' do
+  erb:add_error
+end
+get '/check_error' do
+  erb:check_error
+end
