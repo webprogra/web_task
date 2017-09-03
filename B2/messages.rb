85ec6c834a345df5b5b0094a985ae40f677d7f42
webprogra <1552917447@qq.com>
2017-09-03 12:40:15 +0800

    Update messages.rb

--- B2/messages.rb
+++ B2/messages.rb
@@ -12,15 +12,30 @@ class Messages
   end
   def signup(username,password)
     id=1
-    if User.first==nil
-      user = User.create(id:id,username:username,password:password)
-    else
-      id=User.last.id+1
-      user=User.create(id:id,username:username,password:password)
-      if user==nil
-        return false
-      end
+    user = User.create(username:username,password:password)
+    if user==nil
+      return false
     end
     return true
+  end
+  def add(user_id,message)
+    Message.create(content:message,user_id:user_id,created_at:Time.new)
+  end
+  def search(author)
+    if author==nil||author.length==0
+      return false
+    else
+      user_id=User.find_by(username:author)
+      if user_id==nil
+        return false
+      else
+        messages=Message.where(user_id:user_id)
+        if messages==nil
+          return false
+        else 
+          return messages
+        end
+      end
+    end
   end
 end
