6110b831e58d518cbc96d126523454087878f6ed
webprogra <1552917447@qq.com>
2017-09-03 12:38:59 +0800

    Update main.rb

--- B2/main.rb
+++ B2/main.rb
@@ -4,17 +4,28 @@ require './User.rb'
 require 'sinatra'
 require 'active_record'
 require 'mysql2'
+enable:sessions
 get '/' do
   erb:login
+end
+post '/' do
+  if session[:user_id]==nil
+    erb:exit
+  end
+  @messages=Message.all
+  @username=session[:username]
+  erb:show
 end
 post '/login' do
   message=Messages.new
   id=message.login(params[:username],params[:password])
-  if id==nil 
-    erb:login
+  if id==false
+    erb:login_error
   else
+    session[:username]=params[:username]
     session[:user_id]=id
     @messages=Message.all
+    @username=session[:username]
     erb:show
   end
 end
@@ -23,14 +34,39 @@ get '/signup' do
 end
 post '/signup' do
   message=Messages.new
-  if(params[:username]==nil||params[:password]==nil)
-    erb:signup
+  if(params[:username].length==0||params[:password].length==0)
+    erb:signup_error
   else
     user=message.signup(params[:username],params[:password])
-    if user==nil
-      erb:signup
+    if user==false
+      erb:signup_error
     else
       erb:login
     end
+  end
+end
+post '/add' do
+  if session[:user_id]==nil
+    erb:exit
+  end
+  @username=session[:username]
+  message=Messages.new
+  if(params[:message].length<10)
+    @messages=Message.all
+    erb:add_error
+  else
+    message.add(session[:user_id],params[:message])
+    @messages=Message.all
+    erb:show
+  end
+end
+get '/search' do
+  if session[:user_id]==nil
+    erb:exit
+  else
+    message=Messages.new
+    messages=message.search(params[:author])
+    @messages=messages
+    erb:result
   end
 end
