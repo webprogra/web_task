a8ebbd1caa545ef9f23302dfaebee6e48ea4b150
webprogra <1552917447@qq.com>
2017-09-19 21:24:55 +0800

    Update comment.rb

--- B3/app/model/comment.rb
+++ B3/app/model/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :post
-  has_many:comments
-  validates :title,presences:true,
-                   length: { minimum: 5 }
-  validates :context,presence: true
+  validates :nickname, presence: true,
+                    length: { minimum: 5 }
+  validates :content,presence:true,
+                    length:{minimum:10}
 end
