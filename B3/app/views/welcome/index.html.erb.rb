0149171df5450133a19caf8ca01a67b6f438f17c
webprogra <1552917447@qq.com>
2017-09-19 21:40:43 +0800

    Update index.html.erb.rb

--- B3/app/views/welcome/index.html.erb.rb
+++ B3/app/views/welcome/index.html.erb.rb
@@ -1,2 +1,10 @@
-<h1>Welcome</h1>
-<p><%link_to'My Blog',controller:'admins'%></p>
+<h1>Welcome</h1>  
+<% if user_signed_in? %>  
+    hello!  
+  <%=link_to('my blog',controller:'posts')%>|
+  <%= link_to('登出', destroy_user_session_path, :method => :delete) %>  
+<% else %>  
+  <%= link_to('注册', new_registration_path(:user)) %> | 
+  <%= link_to('登录', new_session_path(:user)) %>
+  <%= link_to('游客',controller:'guests')%>
+<% end %> 
