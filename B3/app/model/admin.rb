a70ff069a50a2a544c5f713ec29ac4eaa7d09334
webprogra <1552917447@qq.com>
2017-09-19 21:24:18 +0800

    Delete admin.rb

--- B3/app/model/admin.rb
+++
@@ -1,6 +0,0 @@
-class Admin < ApplicationRecord
-  has_many:posts,:feedbacks
-  validates :username, presence: true,
-                    length: { minimum: 5 }
-  validates :password,presence: true
-end
